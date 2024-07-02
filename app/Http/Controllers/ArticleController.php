60cee79d3298a0ea9ccc94e464cab061d727d0f1
Nischal <nischalshrestha800@gmail.com>
2024-07-02 12:16:45 +0545

    Managing controller,views,bootstraps

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,4 +6,8 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function show()
+    {
+        return view('list');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/articles', [ArticleController::class, 'show'])->name('articles.show');
